dfbb50bcbb24e8c6a13a1c1e6c66fdf9d1e9e318
Pavel <ps@me.com>
2016-11-17 00:12:45 -0800

    Fix application name

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "safe-garden-48972.herokuapp.com", port: 443],
+  url: [host: "ancient-beyond-94304.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
