1ca2579d898425bd856e2ec528725e9293fcf236
Pavel <ps@me.com>
2016-11-20 22:44:00 -0800

    Add Comeonin. Add more validation

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,9 @@ defmodule Peepchat.UserTest do
 
   alias Peepchat.User
 
-  @valid_attrs %{email: "some content", password_hash: "some content"}
+  @valid_attrs %{email: "ps@zz.com",
+                 password: "some content",
+                 password_confirmation: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -13,6 +15,26 @@ defmodule Peepchat.UserTest do
 
   test "changeset with invalid attributes" do
     changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+
+  test "mis-matched password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "bb@cc.com",
+      password: "one_password",
+      password_confirmation: "another_one"})
+    refute changeset.valid?
+  end
+
+  test "missing password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "bb@cc.com",
+      password: "one_password"})
+    refute changeset.valid?
+  end
+
+  test "short password doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "bb@cc.com",
+      password: "short",
+      password_confirmation: "short"})
     refute changeset.valid?
   end
 end
